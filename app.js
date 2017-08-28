9f15ffe3e91b7f656e249a1aac2c60142013b723
Jay Price <jxprice@live.com>
2017-08-28 15:21:11 -0400

    All changes so far.

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
 console.log('my app is running')
 
+//What are the data types in javascript?
+//primitive data types
+var stringType = 'I am a string'
+var numberType = 9
+var booleanType = true
+var nullType = null
+var undefinedType = undefined
+
+//complex data types
+var objectType = {}
+var arrayType = []
+
+var i=0
+for (i=0; i<3; i++) {
+    console.log(i)
+}
+
+i = 3
+
+while(i > 0) {
+    console.log(i)
+    i--
+}
+
+//conditionals
+if(true){
+    console.log('run this if true')
+} else {
+    console.log('run this if not true')
+}
+
+if( i===0 || i===1 ) {
+    console.log('If one or both of these conditions are true')
+}
+
+if(true) console.log('I am done in one line')
+
+
+
 
